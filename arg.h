1177bfda81920e2c9bd02b5d312dc95e53c0c6e7
Guillaume YE <guillaume.ye@epitech.eu>
2021-10-05 22:25:24 +0200

    Add my_free, set_tab, set_arg
    Notes:
    * my_free: free all dynamic alloc

---
+++ arg.h
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2021
+** live-recursive
+** File description:
+** arg
+*/
+
+#ifndef ARG_H_
+#define ARG_H_
+
+typedef struct arg_s {
+    int rule;
+    int line;
+    int start;
+} arg_t;
+
+#endif /* !ARG_H_ */
--- main.c
+++ main.c
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "arg.h"
 
 char *my_revstr(char *str)
 {
@@ -22,23 +23,22 @@ char *my_revstr(char *str)
     return (str);
 }
 
-
 char *int_to_bin(int nbr)
 {
     char *tab = malloc(sizeof(char) * 9);
-    int i = 0;
     char *stock = malloc(sizeof(char) * 9);
+    int i = 0;
     int a = 0;
 
-    for(i=0; nbr > 0; i++) {
-        tab[i] = nbr%2 + 48;
-        nbr = nbr/2;
+    for (i = 0; nbr > 0; i++) {
+        tab[i] = nbr % 2 + 48;
+        nbr = nbr / 2;
     }
     tab[i] = '\0';
     tab = my_revstr(tab);
     a = strlen(tab);
     i = 0;
-    for (int u = a; a != 8; a++, i++)
+    for (; a != 8; a++, i++)
         stock[i] = '0';
     for (a = 0; a != strlen(tab); a++, i++)
         stock[i] = tab[a];
@@ -47,8 +47,48 @@ char *int_to_bin(int nbr)
     return (stock);
 }
 
+void set_arg(arg_t *arg, char **av)
+{
+    arg->rule = atoi(av[1]);
+    arg->line = atoi(av[2]);
+}
+
+void my_free(char **tab, int lines, arg_t *arg, char *stock)
+{
+    for (int i = 0; i < lines; i++)
+        free(tab[i]);
+    free(tab);
+    free(arg);
+    free(stock);
+}
+
+char **set_tab(char **tab, int lines)
+{
+    tab = malloc(sizeof(char *) * (lines + 1));
+    if (tab == NULL)
+        return NULL;
+    for (int i = 0; i < lines; i++) {
+        tab[i] = malloc(sizeof(char) * (80 + 1));
+        tab[i][80] = '\0';
+    }
+    tab[lines] = NULL;
+    return tab;
+}
+
 int main(int ac, char **av)
 {
-    printf("%s\n", int_to_bin(atoi(av[1])));
+    arg_t *arg = malloc(sizeof(arg_t));
+    char **tab = NULL;
+    char *stock = NULL;
+
+    if (arg == NULL || ac != 3) {
+        fprintf(stderr, "Give me 2 arg please (rule and nb of lines)\n");
+        return 0;
+    }
+    set_arg(arg, av);
+    tab = set_tab(tab, atoi(av[2]));
+    stock = int_to_bin(atoi(av[1]));
+    printf("%s\n", stock);
+    my_free(tab, atoi(av[2]), arg, stock);
     return (0);
 }
