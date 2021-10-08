3c047b18878c3e6f5c74af0b2ddbe84964450d76
Quentin FONKENELL <quentin.fonkenell@epitech.eu>
2021-10-08 19:22:39 +0200

    [ADD] all

--- fir_tree_couleurs.c
+++ fir_tree_couleurs.c
@@ -7,6 +7,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <ncurses.h>
 
 int error_gestion(int ac, char **av)
 {
@@ -49,16 +50,50 @@ int compteSpace(int size, int turn, int loop)
     return (o-size*2+turn-3);
 }
 
+void printColors(char *str)
+{
+    int random = rand() % 100;
+
+    if (random <= 80) {
+        init_pair(1, COLOR_GREEN, COLOR_GREEN);
+        attron(COLOR_PAIR(1));
+    }
+    if (random >= 80 && random <= 85) {
+        init_pair(2, COLOR_MAGENTA, COLOR_MAGENTA);
+        attron(COLOR_PAIR(2));
+    }
+    if (random >= 85 && random <= 90) {
+        init_pair(3, COLOR_RED, COLOR_RED);
+        attron(COLOR_PAIR(3));
+    }
+    if (random >= 90 && random <= 95) {
+        init_pair(4, COLOR_BLUE, COLOR_BLUE);
+        attron(COLOR_PAIR(4));
+    }
+    if (random >= 95 && random <= 100) {
+        init_pair(5, COLOR_WHITE, COLOR_WHITE);
+        attron(COLOR_PAIR(5));
+    }
+    printw(str);
+    attroff(COLOR_PAIR(1));
+    attroff(COLOR_PAIR(2));
+    attroff(COLOR_PAIR(3));
+    attroff(COLOR_PAIR(4));
+    attroff(COLOR_PAIR(5));
+}
+
 int print_part(int size, int turn, int loop, int constSize)
 {
     if (turn <= 0)
         return (loop - 4);
     for (int i = 0; i < ((countLine(constSize)/2) - loop/2); i++)
-        printf(" ");
+        printw(" ");
 
     for (int i = 0; loop != i; i++)
-        putchar('*');
-    printf("\n");
+        printColors("*");
+    refresh();
+
+    printw("\n");
     print_part(size, turn - 1, loop + 2, constSize);
 }
 
@@ -67,10 +102,13 @@ void trunk(int size, int turn)
     if (size == turn)
         return;
     for (int i = 0; i != size * 3; i++)
-        putchar(' ');
+        printw(" ");
+    init_pair(6, COLOR_YELLOW, COLOR_YELLOW);
+    attron(COLOR_PAIR(6));
     for (int i = 0; i != size; i++)
-        putchar('|');
-    putchar('\n');
+        printw("|");
+    attroff(COLOR_PAIR(6));
+    printw("\n");
     trunk(size, turn + 1);
 }
 
@@ -78,22 +116,31 @@ int leaf(int size, int turn, int last, int nb)
 {
     if (size == 0)
         return (0);
-    last = print_part(size, turn + 4, last, nb); // print les étoile
-    leaf(size - 1, turn + 1, last - (turn / 2 * 2), nb); // récursif
+    last = print_part(size, turn + 4, last, nb);
+    leaf(size - 1, turn + 1, last - (turn / 2 * 2), nb);
     return (0);
 }
 
 int fir_tree(int size)
 {
-    leaf(size, 0, 1, size); // algo pour les feuilles
-    trunk(size, 0); // algo pour le tronc
+    leaf(size, 0, 1, size);
+    trunk(size, 0);
     return (0);
 }
 
 int main(int ac, char **av)
 {
+        srand(time(NULL));
     if (error_gestion(ac, av) == 84)
         return (84);
-    fir_tree(atoi(av[1]));
+    initscr();
+    start_color();
+    while (1) {
+        fir_tree(atoi(av[1]));
+        refresh();
+        clear();
+        sleep(1);
+    }
+    getch();
     return (0);
 }
